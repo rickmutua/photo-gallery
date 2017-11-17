b86a91d7725935250c0cfea9ad056bc87511f90d
Erick <rick.mutua12@gmail.com>
2017-11-17 15:25:36 +0300

    update added static files added search for a post functionality added navbar created a requirements file

--- unsplash/me/urls.py
+++ unsplash/me/urls.py
@@ -9,7 +9,7 @@ from . import views
 urlpatterns = [
 
     url(r'^$', views.index, name='index'),
-
+    url(r'^search/', views.search_results, name='search_results'),
 ]
 
 
--- unsplash/me/views.py
+++ unsplash/me/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render, redirect
+from .models import Post
 
 # Create your views here.
 
 
 def index(request):
 
-    return render(request, 'index.html')
+    posts = Post.
+
+    return render(request, 'index.html', {'posts':posts})
+
+
+def search_results(request):
+
+    if 'post' in request.GET and request.GET["post"]:
+        search_term = request.GET.get("post")
+        searched_posts = Post.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html', {"message": message, "posts": searched_posts})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {"message": message})
