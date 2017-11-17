a6174e2897084e5e649441b850729a431cfbdd59
Erick <rick.mutua12@gmail.com>
2017-11-17 11:57:25 +0300

    update update models file to include tags editor and post update the database

--- unsplash/me/admin.py
+++ unsplash/me/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Editor, Post, Tags
 
 # Register your models here.
+
+
+admin.site.register(Editor)
+admin.site.register(Post)
+admin.site.register(Tags)
--- unsplash/me/models.py
+++ unsplash/me/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Editor (models.Model):
+
+    name = models.CharField(max_length=65)
+
+    email = models.EmailField()
+
+    def __str__(self):
+
+        return self.name
+
+
+class Tags(models.Model):
+
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+
+        return self.name
+
+
+class Post(models.Model):
+
+    description = models.CharField(max_length=150)
+
+
+    editor = models.ForeignKey(Editor)
+
+    tags = models.ManyToManyField(Tags)
+
+    pub_date = models.DateTimeField(auto_now_add=True)
