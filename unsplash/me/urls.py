1ed9d2f4e7f56530312c67b6fee74927986c70e2
Erick <rick.mutua12@gmail.com>
2017-11-17 11:21:51 +0300

    update added basic templates created yrl file in me linked and configured me

---
+++ unsplash/me/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from django.conf.urls.static import static
+
+from django.conf import settings
+
+from . import views
+
+
+urlpatterns = [
+
+    url(r'^$', views.index, name='index'),
+
+]
--- unsplash/unsplash/settings.py
+++ unsplash/unsplash/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'me.apps.MeConfig',
+    'bootstrap3'
 ]
 
 MIDDLEWARE = [
